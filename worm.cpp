f81857e5ec96e9481ca9f2d1987c23b87c0e8b0d
amirhoseinkordi <91272570+amirhoseinkordi@users.noreply.github.com>
2021-10-28 16:53:11 +0330

    Create worm.cpp

---
+++ worm.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int n;
+    cout << "worm size:";
+    cin >> n;
+    int a[n];
+
+    for(int i=0;i<n;i++){
+        if(i%2==0){
+        cout << "-";}
+        else{cout << "=";}
+    }   cout << ":>~";
+
+    return 0;
+}
